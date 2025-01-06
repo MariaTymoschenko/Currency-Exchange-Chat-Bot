e7da9d740f1a7051cc3a0f7c67509c358a5c2354
MariaTymoschenko <tymoschenko.maria@gmail.com>
2025-01-07 00:17:43 +0200

    only 2 digits after comma

--- Currency Exchange Chat Bot/src/CsvGraph.jsx
+++ Currency Exchange Chat Bot/src/CsvGraph.jsx
@@ -49,7 +49,7 @@ const CsvGraph = ({fileName}) => {
       <LineChart data={data} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="Date" />
-        <YAxis />
+        <YAxis type="number" domain={[dataMin =>dataMin, dataMax => dataMax ]} />
         <Tooltip />
         <Line type="monotone" dataKey="Close" stroke="#8884d8" />
       </LineChart>
