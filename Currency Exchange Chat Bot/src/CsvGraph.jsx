1ec31679c69ba66cfc2bcbb4228703f793ed8b6c
MariaTymoschenko <tymoschenko.maria@gmail.com>
2025-01-11 19:44:09 +0200

    support exchange from uah

--- Currency Exchange Chat Bot/src/CsvGraph.jsx
+++ Currency Exchange Chat Bot/src/CsvGraph.jsx
@@ -7,31 +7,33 @@ const CsvGraph = ({ fromCurrency, toCurrency }) => {
   const [toCurrencyData, setToCurrencyData] = useState([]);
 
   const fetchCsvData = async (currency, setter) => {
-    try {
-      const response = await fetch(`/exchange_history/Exchange rates - ${currency}.csv`);
-      // console.log(`loaded ${currency}`);
-      const csvText = await response.text();
+    if (currency === "UAH") {
+      setter(Array.from({ length: 365 }, (_, i) => ({ Date: new Date(2024, 0, i + 1), Close: 1.0 })));
+    } else
+      try {
+        const response = await fetch(`/exchange_history/Exchange rates - ${currency}.csv`);
+        const csvText = await response.text();
 
-      Papa.parse(csvText, {
-        header: true,
-        skipEmptyLines: true,
-        delimiter: ",",
-        transform: (value, field)  => {
-          if (field === "Close") {
-            return parseFloat(value.replace(",", "."));
-          } else if (field === "Date") {
-            const [day, month, year] = value.split('.');
-            return new Date(`${year}-${month}-${day}`);
-          }
-          return value;
-        },
-        complete: (result) => {
-          setter(result.data);
-        },
-      });
-    } catch (error) {
-      console.error(`Error fetching or parsing ${currency} CSV file:`, error);
-    }
+        Papa.parse(csvText, {
+          header: true,
+          skipEmptyLines: true,
+          delimiter: ",",
+          transform: (value, field) => {
+            if (field === "Close") {
+              return parseFloat(value.replace(",", "."));
+            } else if (field === "Date") {
+              const [day, month, year] = value.split('.');
+              return new Date(year, month - 1, day);
+            }
+            return value;
+          },
+          complete: (result) => {
+            setter(result.data);
+          },
+        });
+      } catch (error) {
+        console.error(`Error fetching or parsing ${currency} CSV file:`, error);
+      }
   };
 
   useEffect(() => {
@@ -48,22 +50,16 @@ const CsvGraph = ({ fromCurrency, toCurrency }) => {
 
   const combinedData = data.map((row) => {
     let closeRate = row.Close;
-
-   // console.log(`Currency checked ${fromCurrency} ${toCurrency} for`, row);
-
-    // If the toCurrency is not UAH, apply the exchange rate transformation
     if (toCurrency && toCurrency !== "UAH") {
-      // console.log("toCurrencyData", toCurrencyData)
       const toCurrencyRow = toCurrencyData.find(
         (toRow) => toRow.Date.getTime() === row.Date.getTime()
       );
       if (toCurrencyRow) {
-        const toCurrencyClose = toCurrencyRow.Close;
-        closeRate = row.Close / toCurrencyClose; // Combine the exchange rates
-        // console.log(`toCurrencyRow Found ${closeRate}`);
+        closeRate = row.Close / toCurrencyRow.Close;
       }
       else {
-        // console.log(`toCurrencyRow not found ${row.Date}`);
+        console.log(`toCurrencyRow not found ${row.Date}`);
+        closeRate = 0;
       }
     }
 
@@ -71,17 +67,17 @@ const CsvGraph = ({ fromCurrency, toCurrency }) => {
       ...row,
       Close: closeRate,
     };
-  });
+  }).filter((row) => { return row.Close > 0 });
 
   return (
     <ResponsiveContainer width="100%" height={400}>
       <LineChart data={combinedData.map((row) => ({
-            ...row,
-            Date: new Date(row.Date).toLocaleDateString("uk-UA"),
-          }))} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
+        ...row,
+        Date: new Date(row.Date).toLocaleDateString("uk-UA"),
+      }))} margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="Date" />
-        <YAxis type="number" domain={[dataMin =>dataMin, dataMax => dataMax ]} />
+        <YAxis type="number" domain={[dataMin => dataMin, dataMax => dataMax]} />
         <Tooltip />
         <Line type="monotone" dataKey="Close" stroke="#8884d8" />
       </LineChart>
